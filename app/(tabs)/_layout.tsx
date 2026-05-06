3c052824c738b60be628c92c1023951927c31778
Bhagwan jha <analysthacker03@gmail.com>
2026-05-06 08:55:31 +0530

    navigation setup

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,62 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable no-unused-expressions */
+import { tabs } from "@/data";
+import { Tabs } from "expo-router";
+import { View, Image } from "react-native";
+import clsx from 'clsx'
+import { colors,components } from "@/constants/theme";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+const tabBar = components.tabBar
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+const TabLayout = () => {
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+    const insets = useSafeAreaInsets()
+
+    const TabIcon = ({focused,icon}: TabIconProps) => {
+        return (
+            <View className="tabs-icon">
+                <View className={clsx('tabs-pill',focused && 'tabs-active')}>
+                    <Image resizeMode="contain" source={icon} className="tabs-glyp" />
+                </View>
+            </View>
+        )
+    }
+
+    <Tabs screenOptions={{
         headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+        tabBarShowLabel: false,
+        tabBarStyle:{
+            position: 'absolute',
+            bottom: Math.max(insets.bottom,tabBar.horizontalInset),
+            height: tabBar.height,
+            marginHorizontal: tabBar.horizontalInset,
+            borderRadius: tabBar.radius,
+            backgroundColor: colors.primary,
+            borderTopWidth: 0,
+            elevation: 0
+        },
+        tabBarItemStyle: {
+            paddingVertical: tabBar.height / 2 - tabBar.iconFrame / 1.6
+        },
+        tabBarIconStyle: {
+            width: tabBar.iconFrame,
+            height: tabBar.iconFrame,
+            alignItems: 'center'
+        }
+    }}>
+        {tabs.map((tab) => (
+            <Tabs.Screen 
+            key={tab.name} 
+            name={tab.name}
+            options={{ 
+                title: tab.title,
+                tabBarIcon: ({focused}) => {
+                    <TabIcon focused={focused} icon={tab.icon} />
+                }
+             }} />
+        ))} 
     </Tabs>
-  );
 }
+
+export default TabLayout;
---
+++ app/subscriptions/spotify.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const spotify = () => {
+  return (
+    <View>
+      <Text>spotify</Text>
+    </View>
+  )
+}
+
+export default spotify
+
+const styles = StyleSheet.create({})
